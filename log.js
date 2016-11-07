58e1446a3018bba71d43ca4b45049186a6d3efec
san <san@sancomp>
2016-11-07 22:46:30 +0300

    log

--- log.js
+++ log.js
@@ -1,14 +1,16 @@
 module.exports = (obj) => {
-  const prefix = (new Date).toISOString() + '    ';
-  let out = '';
-  if (typeof obj === 'string') {
-    out = obj.slice();
-  } else if (typeof obj === 'object') {
-    if (obj instanceof Error) {
-      out = 'Error: ' + obj.message;
-    } else {
-      out = JSON.stringify(obj);
+  if (obj) {
+    const prefix = (new Date).toISOString() + '    ';
+    let out = '';
+    if (typeof obj === 'string') {
+      out = obj.slice();
+    } else if (typeof obj === 'object') {
+      if (obj instanceof Error) {
+        out = 'Error: ' + obj.message;
+      } else {
+        out = JSON.stringify(obj);
+      }
     }
+    console.log(prefix + out);
   }
-  console.log(prefix + out);
 }
